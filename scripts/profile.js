1570cb3326eba90f51a9239e5c19126d2e2d88d7
Maximilian Alexander <max@zinkpulse.com>
2015-08-28 11:07:06 -0400

    Fixing broken links to images

--- scripts/profile.js
+++ scripts/profile.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
     new Medium({
         element: document.getElementById('profile-desc')
     });
+
+    new Medium({
+        element: document.getElementById('profile-name')
+    });
+
+    new Medium({
+        element: document.getElementById('profile-tagline')
+    })
 });
